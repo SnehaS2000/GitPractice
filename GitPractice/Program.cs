c3ddcdb09b535233867f3299e04ea5cf9ea5914d
Sneha S <Sneha.S@lntinfotech.com>
2022-11-25 17:11:23 +0530

    dev second commit

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("develop Branch");
+Console.WriteLine("develop Branch");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("develop Branch");
 Console.WriteLine("develop Branch");
